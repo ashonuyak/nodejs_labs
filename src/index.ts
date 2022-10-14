bac26dc0a18147f495c0dbe31cb771fa19d33b62
ashonuyak <ashonuyak@gmail.com>
2022-10-14 18:00:30 +0300

    change index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,9 @@
-const alo = 'asd'
-console.log(alo)
-console.log(`Server is running on port ${process.env.PORT}`)
+import http from 'http'
+// Read Environment Parameters
+const port = Number(process.env.PORT || 3001)
+const greeting = 'Good evening, Mr Nosov'
+const server = http.createServer((request, response) => {
+  response.writeHead(200, { 'Content-Type': 'text/plain' })
+  response.end(`${greeting}\n`)
+})
+server.listen(port)
