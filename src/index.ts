add3f9bb132a5c0318fb6e9ed09f01f719601680
ashonuyak <ashonuyak@gmail.com>
2022-10-16 13:15:26 +0300

    fix due to review

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import http from 'http'
 // Read Environment Parameters
-const port = Number(process.env.PORT || 3001)
+const port = Number(process.env.PORT) || 3001
 const greeting = 'Good evening, Mr Nosov'
 const server = http.createServer((request, response) => {
   response.writeHead(200, { 'Content-Type': 'text/plain' })
